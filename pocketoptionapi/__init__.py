27ccbb3394bf75ca6696fd1e287a4607d97bc297
ByMyselfJhones <bymyselfjhones@gmail.com>
2025-07-03 02:40:02 -0300

    Adicionando Arquivos

---
+++ pocketoptionapi/__init__.py
@@ -0,0 +1,13 @@
+"""
+# Autor: ByMyselfJhones
+# Função: Inicialização do pacote PocketOptionAPI
+# Descrição:
+# - Define a versão do pacote (0.1.0)
+# - Disponibiliza a classe PocketOptionAPI para importação
+"""
+
+from .api import PocketOptionAPI
+
+__version__ = "0.1.0"
+
+__all__ = ['PocketOptionAPI']
